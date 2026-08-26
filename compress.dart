0afca19143bc87a56d734fb90f754eb03b51bb48
asav-de <andrii_savchenko@teilnehmer.cdemy.de>
2026-08-26 17:22:56 +0200

    String compress is completed

--- compress.dart
+++ compress.dart
@@ -1,7 +1,44 @@
+import 'greeter.dart' show isAlphaDe;
+//import 'greeter.dart' show isDigit;
 void main(List<String> args) {
-  args.isEmpty ? print('args is empty') : compress(args);
+  List<String> result = [];
+  args.isEmpty ? result.add("") : compress(args, result);
+  print(result);
 }
 
-compress(List<String> args) {
-  print(args.map((a) => a.length));
+compress(List<String> args, result) {
+  int currentLetterCount = 0;
+  final String input = args.join('');
+  String prevLetter = input[0].toUpperCase();
+
+  for (final c in input.runes){
+    final ch = String.fromCharCode(c).toUpperCase();
+
+    if (!isAlphaDe(ch)){
+      print('Invalid Input!');
+      break;
+    }
+    if (isAlphaDe(ch) && prevLetter == ch){
+      currentLetterCount++;
+    } else if (prevLetter != ch && currentLetterCount < 3){
+      for (int i = 0; i < currentLetterCount; i++){
+        result.add(prevLetter);
+      }
+      prevLetter = ch;
+      currentLetterCount = 1;
+    } else if (prevLetter != ch && currentLetterCount >= 3) {
+      result.add(prevLetter);
+      result.add(currentLetterCount.toString());
+      prevLetter = ch;
+      currentLetterCount = 1;
+    }
+  }
+  if (currentLetterCount < 3){
+    for (int i = 0; i < currentLetterCount; i++){
+      result.add(prevLetter);
+    }
+  } else {
+    result.add(prevLetter);
+    result.add(currentLetterCount.toString());
+  }
 }
